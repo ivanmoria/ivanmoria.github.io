db825cd45e3b43a02c59b116178dc405949c09b0
ivanmoria <ivanmoriabr@gmail.com>
2024-06-13 15:53:40 -0300

    atual

--- script.js
+++ script.js
@@ -1,50 +1,13 @@
-// Código de Processing
-var num = 2000;
-var range = 6;
 
-var ax = new Array(num);
-var ay = new Array(num); 
-
-function setup() {
-  var canvas = createCanvas(windowWidth, windowHeight); // Defina o tamanho do canvas como a largura e altura da janela do navegador
-  canvas.parent('canvas'); // Define o elemento pai do canvas
-  noFill(); // Sem preenchimento nos retângulos
-  stroke(255); // Cor da linha (branca)
-  rectMode(CENTER); // Centro dos retângulos
-
-  for(var i = 0; i < num; i++) {
-    ax[i] = width/2;
-    ay[i] = height/2;
-  }
-  frameRate(30);
-}
-
-function draw() {
-  background(91); // Cor de fundo cinza escuro
-
-  // Shift all elements 1 place to the left
-  for(var i = 1; i < num; i++) {
-    ax[i-1] = ax[i];
-    ay[i-1] = ay[i];
-  }
-
-  // Put a new value at the end of the array
-  ax[num-1] += random(-range, range);
-  ay[num-1] += random(-range, range);
-
-  // Constrain all points to the screen
-  ax[num-1] = constrain(ax[num-1], 0, width);
-  ay[num-1] = constrain(ay[num-1], 0, height);
-  
-  // Draw a line connecting the points
-  for(var i=1; i<num; i++) {    
-    var val = float(i)/num * 204.0 + 51;
-    stroke(val);
-    line(ax[i-1], ay[i-1], ax[i], ay[i]);
-  }
-}
 
 // Evento de clique no botão
 document.getElementById('btn').addEventListener('click', function() {
-  alert('Olá! Você clicou no botão.');
+  alert('Olá! Eu ainda vou inserir algo aqui.');
+});
+document.addEventListener('DOMContentLoaded', function () {
+  const themeSwitch = document.getElementById('dark-mode-switch');
+
+  themeSwitch.addEventListener('change', function () {
+      document.body.classList.toggle('dark-theme', themeSwitch.checked);
+  });
 });
