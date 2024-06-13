d3d0e5a4a189de6e0c1ed1401f15f4b94f57a4a3
ivanmoria <ivanmoriabr@gmail.com>
2024-06-13 17:38:43 -0300

    os

--- script.js
+++ script.js
@@ -4,10 +4,5 @@
 document.getElementById('btn').addEventListener('click', function() {
   alert('Olá! Eu ainda vou inserir algo aqui.');
 });
-document.addEventListener('DOMContentLoaded', function () {
-  const themeSwitch = document.getElementById('dark-mode-switch');
 
-  themeSwitch.addEventListener('change', function () {
-      document.body.classList.toggle('dark-theme', themeSwitch.checked);
-  });
-});
+
