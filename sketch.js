237bf4c12610cbca08c95d885d3f7ce186472a1c
Ivan Moriá Borges <167134271+ivanmoria@users.noreply.github.com>
2024-06-12 23:21:42 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,48 +1,47 @@
-/**
- * Brownian motion. 
- * 
- * Recording random movement as a continuous line. 
- */
- 
-int num = 2000;
-int range = 6;
-
-float[] ax = new float[num];
-float[] ay = new float[num]; 
-
-
-void setup() 
-{
-  size(640, 360);
-  for(int i = 0; i < num; i++) {
-    ax[i] = width/2;
-    ay[i] = height/2;
-  }
-  frameRate(30);
+// Código de Processing
+function setup() {
+    var canvas = createCanvas(windowWidth, windowHeight); // Defina o tamanho do canvas como a largura e altura da janela do navegador
+    canvas.position(0, 0); // Posicione o canvas no canto superior esquerdo
+    canvas.style('z-index', '-1'); // Coloque o canvas no fundo da página
+    noStroke(); // Sem contorno nos retângulos
+    rectMode(CENTER); // Centro dos retângulos
 }
-
-void draw() 
-{
-  background(51);
   
-  // Shift all elements 1 place to the left
-  for(int i = 1; i < num; i++) {
-    ax[i-1] = ax[i];
-    ay[i-1] = ay[i];
-  }
-
-  // Put a new value at the end of the array
-  ax[num-1] += random(-range, range);
-  ay[num-1] += random(-range, range);
-
-  // Constrain all points to the screen
-  ax[num-1] = constrain(ax[num-1], 0, width);
-  ay[num-1] = constrain(ay[num-1], 0, height);
+function draw() {
+    background(51); // Cor de fundo branca
   
-  // Draw a line connecting the points
-  for(int i=1; i<num; i++) {    
-    float val = float(i)/num * 204.0 + 51;
-    stroke(val);
-    line(ax[i-1], ay[i-1], ax[i], ay[i]);
-  }
+    fill(0); // Cor preta dos retângulos
+  
+    // Variáveis para o sketch de Brownian motion
+    var num = 2000;
+    var range = 6;
+    var ax = new Array(num);
+    var ay = new Array(num);
+
+    // Inicialização dos arrays
+    for(var i = 0; i < num; i++) {
+        ax[i] = width/2;
+        ay[i] = height/2;
+    }
+
+    // Shift all elements 1 place to the left
+    for(var i = 1; i < num; i++) {
+        ax[i-1] = ax[i];
+        ay[i-1] = ay[i];
+    }
+
+    // Put a new value at the end of the array
+    ax[num-1] += random(-range, range);
+    ay[num-1] += random(-range, range);
+
+    // Constrain all points to the screen
+    ax[num-1] = constrain(ax[num-1], 0, width);
+    ay[num-1] = constrain(ay[num-1], 0, height);
+
+    // Draw a line connecting the points
+    for(var i=1; i<num; i++) {    
+        var val = float(i)/num * 204.0 + 51;
+        stroke(val);
+        line(ax[i-1], ay[i-1], ax[i], ay[i]);
+    }
 }
