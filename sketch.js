ea6f5f636698d2ff7a7e3179d00996de419ab831
Ivan Moriá Borges <167134271+ivanmoria@users.noreply.github.com>
2024-06-12 23:12:41 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
-/**
- * Mouse 2D. 
- * 
- * Moving the mouse changes the position and size of each box. 
- */
+// Código de Processing
 function setup() {
-  createCanvas(640, 360); 
-  noStroke();
-  rectMode(CENTER);
+    var canvas = createCanvas(400, 400); // Defina o tamanho do canvas
+    canvas.parent("canvas"); // Define o pai do canvas como o elemento com id "canvas"
+    noStroke(); // Sem contorno nos retângulos
+    rectMode(CENTER); // Centro dos retângulos
 }
-
+  
 function draw() {
-  background(51); 
-  fill(255, 204);
-  rect(mouseX, height/2, mouseY/2+10, mouseY/2+10);
-  fill(255, 204);
-  var inverseX = width-mouseX;
-  var inverseY = height-mouseY;
-  rect(inverseX, height/2, (inverseY/2)+10, (inverseY/2)+10);
+    background(51); // Cor de fundo
+  
+    fill(255, 204); // Cor dos retângulos
+  
+    // Retângulo que segue o mouse horizontalmente
+    rect(mouseX, height/2, mouseY/2+10, mouseY/2+10);
+  
+    // Retângulo que segue o mouse invertido horizontalmente
+    var inverseX = width-mouseX;
+    var inverseY = height-mouseY;
+    rect(inverseX, height/2, (inverseY/2)+10, (inverseY/2)+10);
 }
