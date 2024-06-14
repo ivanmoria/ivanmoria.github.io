eead95162b53e87c45b9c42c9fabb2a9dd7ac85c
ivanmoria <ivanmoriabr@gmail.com>
2024-06-14 01:40:48 -0300

    refresh

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-
-
 // Evento de clique no botão
 document.getElementById('btn').addEventListener('click', function() {
   alert('Olá! Eu ainda vou inserir algo aqui.');
 });
 
-
 document.getElementById('loading').style.display = 'block';
-fetch(url)
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('content').innerHTML = html;
-        document.getElementById('loading').style.display = 'none';
-    });
 
-  
-  
+function loadContent(url) {
+  fetch(url)
+      .then(response => response.text())
+      .then(html => {
+          document.getElementById('content').innerHTML = html;
+          document.getElementById('loading').style.display = 'none';
+          // Adicionando a classe 'loaded' após o conteúdo carregar completamente
+          setTimeout(function() {
+              document.getElementById('content').classList.add('loaded');
+          }, 50); // Tempo de espera em milissegundos (50ms)
+      });
+}
+
+// Função para carregar o conteúdo com uma transição suave
+function transitionPage(url) {
+  document.getElementById('content').classList.remove('loaded');
+  document.getElementById('loading').style.display = 'block';
+  setTimeout(function() {
+      loadContent(url);
+  }, 700); // Tempo de espera em milissegundos (200ms)
+}
+
+// Exemplo de uso para carregar uma nova página
+transitionPage('projects.html');
