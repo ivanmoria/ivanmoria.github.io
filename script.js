4715ef11fa3d72eb02ce93d1fdd4a9d4e38d94a7
ivanmoria <ivanmoriabr@gmail.com>
2024-06-15 19:32:12 -0300

    docs

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ function loadContent(url) {
       });
 }
 
+
+function downloadFile(fileUrl, fileName) {
+  var a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
