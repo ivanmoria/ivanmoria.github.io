58ec2a88fef2b8b5d22769e187df52bec48e222a
ivanmoria <ivanmoriabr@gmail.com>
2024-06-25 00:40:26 -0300

    videos

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// Evento de clique no botão
-document.getElementById('btn').addEventListener('click', function() {
-  alert('Olá! Eu ainda vou inserir algo aqui.');
-});
-
-document.getElementById('loading').style.display = 'block';
-
-function loadContent(url) {
-  fetch(url)
-      .then(response => response.text())
-      .then(html => {
-          document.getElementById('content').innerHTML = html;
-          document.getElementById('loading').style.display = 'none';
-          // Adicionando a classe 'loaded' após o conteúdo carregar completamente
-          setTimeout(function() {
-              document.getElementById('content').classList.add('loaded');
-          }, 50); // Tempo de espera em milissegundos (50ms)
-      });
-}
 
 
 function downloadFile(fileUrl, fileName) {
