ff22f238a483376e1dc5fc2fdd63d73b99de5617
ivanmoria <ivanmoriabr@gmail.com>
2024-06-29 10:21:56 -0300

    layout assinatura

--- script.js
+++ script.js
@@ -10,10 +10,22 @@ function downloadFile(fileUrl, fileName) {
 }
 
 
-<script>
 
-function toggleOptions() {
-  const container = document.querySelector('.setting-container');
-  container.classList.toggle('open');
+function adjustSignatureText() {
+  var assinatura = document.getElementById('texto-animado');
+  var container = document.querySelector('.assinatura-container');
+  
+  if (window.innerWidth < 321 || window.innerHeight< 568) { // Ajuste o valor conforme necessário
+    assinatura.textContent = 'IMBR';
+    container.classList.add('adjusted');
+  } else {
+    assinatura.textContent = 'Ivan Moriá Borges Rodrigues';
+    container.classList.remove('adjusted');
+  }
 }
-</script>
+
+// Chama a função quando a janela é redimensionada
+window.onresize = adjustSignatureText;
+
+// Chama a função quando a página carrega
+window.onload = adjustSignatureText;
