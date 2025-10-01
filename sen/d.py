db4206202f4c0c5ba0d86cc86c784bd94628a440
Ivan Moriá Borges <ivanmoriabr@gmail.com>
2025-10-01 17:41:49 -0300

    Aplicando .gitignore para ignorar arquivos

--- sen/d.py
+++
@@ -1,8 +0,0 @@
-from moviepy.editor import VideoFileClip
-
-def gif_para_mp4(caminho_gif, caminho_mp4):
-    clip = VideoFileClip(caminho_gif)
-    clip.write_videofile(caminho_mp4, codec='libx264', audio=False)
-
-# Exemplo de uso:
-gif_para_mp4("/Users/ivanmoria/Desktop/gif/masssster.gif", "/Users/ivanmoria/Desktop/gif/masssster.mp4")
